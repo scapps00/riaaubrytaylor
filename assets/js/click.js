682df5330be18771b7bcce09d289ba7041dc3719
scapps00 <hello@samanthacapps.com>
2018-06-02 15:04:28 -0400

    left eye fully working

--- assets/js/click.js
+++ assets/js/click.js
@@ -16,13 +16,46 @@ $(".links").click(function(event) {
         var lengthY = originY1 - hitY;
     }
     var length = Math.sqrt(Math.pow(lengthY, 2) + Math.pow(hitX - originX1, 2));
-    $("#laser1").width(length);
     $("#laser1").height(5);
     $("#laser1").css("left", originX1.toString() + "px");
     $("#laser1").css("top", originY1.toString() + "px");
     var angle = Math.acos((hitX - originX1) / length);
-    console.log(angle);
-    console.log(length);
-    console.log(hitX - originX1);
-    $("#laser1").css("transform", "rotate(" + angle.toString() + "rad)");
+    if (hitY >= originY1) {
+        $("#laser1").css("transform", "rotate(" + angle.toString() + "rad)");
+    } else {
+        $("#laser1").css("transform", "rotate(" + (2 * Math.PI - angle).toString() + "rad)");
+    }
+    sendLaser(length);
 });
+
+function sendLaser(length) {
+    $("#laser1").width(length * .1);
+    setTimeout(function() {
+        $("#laser1").width(length * .2);
+    }, 100);
+    setTimeout(function() {
+        $("#laser1").width(length * .3);
+    }, 200);
+    setTimeout(function() {
+        $("#laser1").width(length * .4);
+        document.getElementById("laserSound").play();
+    }, 300);
+    setTimeout(function() {
+        $("#laser1").width(length * .5);
+    }, 400);
+    setTimeout(function() {
+        $("#laser1").width(length * .6);
+    }, 500);
+    setTimeout(function() {
+        $("#laser1").width(length * .7);
+    }, 600);
+    setTimeout(function() {
+        $("#laser1").width(length * .8);
+    }, 700);
+    setTimeout(function() {
+        $("#laser1").width(length * .9);
+    }, 800);
+    setTimeout(function() {
+        $("#laser1").width(length);
+    }, 900);
+}
